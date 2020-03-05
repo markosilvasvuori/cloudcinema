44d53d92767e9a15417fd99bc8fccf45bf8aea8c
Marko Silvasvuori <marko.silvasvuori@gmail.com>
2020-03-05 21:41:29 +0200

    Added API calls for "upcoming", "popular" and "now playing" movies

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 const headerNav = document.querySelector('#search');
 const accountButton = document.querySelector('.account-button');
+const baseUrl = 'https://image.tmdb.org/t/p/'
+const api = '93530160840d922e585f6b81bf62a7a0'
 let searchValue = '';
 
 // Search elements
@@ -28,4 +30,83 @@ searchInput.onblur = function() {
     accountButton.style.display = 'block';
     searchValue = searchInput.value;
     searchInput.value = '';
+}
+
+// API
+window.onload = () => {
+    getUpcomingMovies();
+    getPopularMovies();
+    getNowPlayingMovies();
+}
+
+function getUpcomingMovies() {
+    const upcomingMovies = document.querySelector('#upcoming ul');
+    let output = '';
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', `https://api.themoviedb.org/3/movie/upcoming?api_key=${api}&language=en-US&page=1`, true);
+
+    xhr.onload = function() {
+        if (xhr.status == 200) {
+            const movies = JSON.parse(xhr.responseText);
+            console.log(movies)
+
+            for (let i = 0; i < 5; i += 1) {
+                let poster = baseUrl + 'w500' + movies.results[i].poster_path
+                output += `<li><img src="${poster}" alt=""></li>`
+            }
+
+            upcomingMovies.innerHTML += output;
+        }
+    }
+
+    xhr.send();
+}
+
+function getPopularMovies() {
+    const popularMovies = document.querySelector('#popular ul');
+    let output = '';
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', `https://api.themoviedb.org/3/movie/popular?api_key=${api}&language=en-US&page=1`, true);
+
+    xhr.onload = function() {
+        if (xhr.status == 200) {
+            const movies = JSON.parse(xhr.responseText);
+            console.log(movies);
+
+            for (let i = 0; i < 5; i += 1) {
+                let poster = `${baseUrl}w500${movies.results[i].poster_path}`;
+                output += `<li><img src="${poster}" alt=""></li>`;
+            }
+
+            popularMovies.innerHTML += output;
+        }
+    }
+
+    xhr.send();
+}
+
+function getNowPlayingMovies() {
+    const nowPlayingMovies = document.querySelector('#now-playing ul');
+    let output = '';
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', `https://api.themoviedb.org/3/movie/now_playing?api_key=${api}&language=en-US&page=1`, true);
+
+    xhr.onload = function() {
+        if (xhr.status == 200) {
+            const movies = JSON.parse(xhr.responseText);
+            console.log(movies);
+
+            for (let i = 0; i < 5; i += 1) {
+                let poster = `${baseUrl}w500${movies.results[i].poster_path}`;
+                output += `<li><img src="${poster}" alt=""></li>`;
+            }
+
+            nowPlayingMovies.innerHTML += output;
+        }
+    }
+
+    xhr.send();
 }
